26baa7867bf7f90307b454895f21a8e6c044472e
ucEzette <u.ezette@alustudent.com>
2024-10-23 14:23:59 -0700

    commit 3 print alphabet

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ascii_char in range(97, 123):
-    if ascii_char != 101 and ascii_char != 113:  # Exclude 'e' (ASCII 101) and 'q' (ASCII 113)
-        print("{}".format(chr(ascii_char)), end="")
+    if ascii_char != 101 and ascii_char != 113:
+        print("{}".format(chr(ascii_char)), end=" ")
