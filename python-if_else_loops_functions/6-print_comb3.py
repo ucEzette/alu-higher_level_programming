c2c284e50bde731f6e0cf12c8e4ce98f46e141b2
ucEzette <u.ezette@alustudent.com>
2024-10-23 15:02:20 -0700

    print combo commit

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for num1 in range(10):
+
+for num1 in range(0, 10):
     for num2 in range(num1 + 1, 10):
-        if num1 < 8 or (num1 == 8 and num2 == 9):
-            print("{:2d} {:2d}, ".format(num1, num2), end=", ")
+        if num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
         else:
-            print("{:2d} {:2d}".format(num1, num2))
+            print("{}{}".format(num1, num2), end=", ")
