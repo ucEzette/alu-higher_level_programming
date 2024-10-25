0a0305a69d5a43028f470f454115f9e171468f80
ucEzette <u.ezette@alustudent.com>
2024-10-25 07:43:16 -0700

    error fixed

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i, element in enumerate(row):
-            # Print element with str.format(), and avoid trailing space at the end of each row
-            print("{:d}".format(element), end=" " if i < len(row) - 1 else "")
-        print()  # Move to the next line after each row
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
