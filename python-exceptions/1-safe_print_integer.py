a802fb85b335bfdaec6eb6b5c399574937e7dd68
ucEzette <u.ezette@alustudent.com>
2024-11-06 01:33:10 -0800

    fixed whitespace for PEP8 compliance

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,4 +4,4 @@ def safe_print_integer(value):
         print("{:d}".format(value))  # Attempt to format and print
         return True  # Return True if no error occurs
     except (ValueError, TypeError):
-        return False  # Return False if formatting fails 
+        return False  # Return False if formatting fails
