8ec6a62edcdde011e6ebee2dcd1c79f1387738da
ucEzette <u.ezette@alustudent.com>
2024-11-06 01:30:12 -0800

    alter and commit

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))  # Attempt to format and print the value as an integer
+        print("{:d}".format(value))  # Attempt to format and print
         return True  # Return True if no error occurs
     except (ValueError, TypeError):
-        return False  # Return False if formatting fails (i.e., value isn't an integer)
+        return False  # Return False if formatting fails 
