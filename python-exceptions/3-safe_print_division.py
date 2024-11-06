b1b0b8dd657e180d68fc159a0314c02fb706a2aa
ucEzette <u.ezette@alustudent.com>
2024-11-06 01:50:14 -0800

    fixed whitespace for PEP8 compliance

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b  # Attempt to divide a by b
     except ZeroDivisionError:
-        result = None  # Set result to None if division by zero occurs
+        result = None
     finally:
-        print("Inside result: {}".format(result))  # Print result in the finally block
-    return result  # Return the result of the division or None
+        print("Inside result: {}".format(result))  # Print result
+    return result
