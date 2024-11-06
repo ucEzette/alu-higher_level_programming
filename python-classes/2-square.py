f0b682194912fe3aa6b80ba67a470a68c5a26dd4
ucEzette <u.ezette@alustudent.com>
2024-11-06 02:54:45 -0800

    based on 1-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,6 +4,7 @@
     Define a class 'Square' that defines a square.
 """
 
+
 class Square:
     """
     Square class defines a square with private size attribute.
@@ -17,5 +18,5 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+
         self.__size = size
