3e96ea724d9c6be5fd19a35c5ec17c133a015fe3
ucEzette <u.ezette@alustudent.com>
2024-11-13 19:47:46 -0800

    first commit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
+"""
+This module provides a function to read and print the content of a text file.
+"""
+
+
 def read_file(filename=""):
     """
-    Reads a text file (UTF-8) and prints its content to stdout.
+    Reads a text file (UTF8) and prints it to stdout.
 
     Args:
-        filename (str): The path to the text file to be read. Defaults to an empty string, which can be set
-                        to a valid file path by the caller.
-
-    Behavior:
-        - Opens the specified file in read mode with UTF-8 encoding.
-        - Reads and prints the entire content of the file to stdout.
-        - Uses the `with` statement to ensure the file is closed automatically.
-        - Does not handle file permission issues or missing file errors.
-
-    Example:
-        >>> read_file("example.txt")
-        (Prints the content of "example.txt" to stdout)
-
-    Note:
-        This function assumes the file exists and is accessible with read permissions.
-        It also assumes UTF-8 encoding for the text file.
-
+        filename (str): The name of the file to read.
     """
     with open(filename, "r", encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
