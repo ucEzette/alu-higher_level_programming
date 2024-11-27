f6dc1b570bd4b376acb2efa99cb684b94c02f066
ucEzette <u.ezette@alustudent.com>
2024-11-27 01:19:07 -0800

    changed approach

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Fetches url"""
-import urllib.request
+"""
+Python script that fetches https://alu-intranet.hbtn.io/status
+"""
+import requests
 
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as req:
-        body = req.read()
-        # Display response body
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body.decode("utf-8")))
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
