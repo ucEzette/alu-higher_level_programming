3735bb052849bc05c04a69a6f3b74599404174bb
ucEzette <u.ezette@alustudent.com>
2024-11-27 00:54:42 -0800

    changed approach

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Fetches url"""
-import requests
-
+import urllib.request
 
 if __name__ == "__main__":
-    req = requests.get("https://alu-intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format((req.text)))
+    url = "https://alu-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as req:
+        body = req.read()
+        # Display response body
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body.decode("utf-8")))
