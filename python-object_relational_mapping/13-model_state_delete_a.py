2fa3afec808dfb06074f49df38968d27221da87e
ucEzette <u.ezette@alustudent.com>
2024-12-01 04:25:10 -0800

    last fix

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Deletes all State objects with a name containing the letter 'a' 
+Deletes all State objects with a name containing the letter 'a'
 from the database hbtn_0e_6_usa.
 """
 
@@ -15,14 +15,13 @@ if __name__ == "__main__":
     # Establish a connection to the database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    
+
     # Create the session
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # Query all State objects with a name containing the letter 'a'
     states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
-
     # Delete each state in the result
     for state in states_to_delete:
         session.delete(state)
