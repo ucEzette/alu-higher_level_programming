96e909283d5d78eca4a73a128d2ae79ab0225e00
ucEzette <u.ezette@alustudent.com>
2024-12-01 04:46:25 -0800

    last fix

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -13,15 +13,21 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     # Establish a connection to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1], argv[2], argv[3]
+        ),
+        pool_pre_ping=True
+    )
 
     # Create the session
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # Query all State objects with a name containing the letter 'a'
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    states_to_delete = session.query(State).filter(
+        State.name.like('%a%')
+    ).all()
 
     # Delete each state in the result
     for state in states_to_delete:
